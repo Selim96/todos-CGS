7ebcf9bfeeb99efc37c5bf8ddb47068119fb4b0b
Selim Fazylov <selim.fazylov1@gmail.com>
2023-02-10 22:39:20 +0200

    add modal files

---
+++ src/modules/AddTodoModal/AddTodoModal.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AddTodoModal: React.FC = () => {
+    return (
+        <div>add modal</div>
+    )
+};
+
+export default AddTodoModal;
---
+++ src/modules/Modals/Modals.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const modalRoot = document.querySelector('#modal-root');
+
+const Modals: React.FC = ({children, }) => {
+    
+};
+
+export default Modals;
